49cc7a8da59130d6ef9f4f716a5d59fff1ccf118
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-11 12:49:24 +0530

    Exercise question of Day 1 completed

---
+++ Python_Boss_Coder/Day_1/Example_Question_2.py
@@ -0,0 +1,8 @@
+# Q 2: Calculate the sum of two numbers entered by the user.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = num1 + num2
+
+print(f"Addition of {num1} and {num2} is {result}.")
---
+++ Python_Boss_Coder/Day_1/Example_Question_3.py
@@ -0,0 +1,7 @@
+# Q 3: Convert temperature from Celsius to Fahrenheit.
+
+celcius = float(input("Enter temperature in celcius: "))
+
+farenhite = (9/5 * celcius) + 32
+
+print(f"Celcius: {celcius}C is {farenhite}F.")
