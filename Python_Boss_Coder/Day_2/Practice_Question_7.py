142f023b6a184b67a312bed27ff679307ddf99c8
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-12 19:30:30 +0530

    Practice question 7 of day 2 completed

---
+++ Python_Boss_Coder/Day_2/Practice_Question_7.py
@@ -0,0 +1,7 @@
+# Calculate the area and circumference of a circle given its radius.
+
+radius = float(input("Enter radius to find circumference of circle: "))
+pi = 3.14
+
+ci = 2 * pi * radius
+print(f"Circumference: {ci}")
