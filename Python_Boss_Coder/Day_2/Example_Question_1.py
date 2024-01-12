c58b9aa6793be3f79a724ae397e5eb5ec0092143
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-12 15:50:27 +0530

    Exercise question 1 of day 2 completed

---
+++ Python_Boss_Coder/Day_2/Example_Question_1.py
@@ -0,0 +1,7 @@
+# Create variables for storing a person's name, age, and average test score.
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+average = float(input("Enter aveg test score: "))
+
+print(f"Name: {name} Age: {age} Average: {average}")
