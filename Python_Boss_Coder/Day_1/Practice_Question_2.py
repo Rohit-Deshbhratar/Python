08ab0ec722951a8525c6ae7f366af5bcfb17a274
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-12 09:29:02 +0530

    Practice question of day 1 completed

---
+++ Python_Boss_Coder/Day_1/Practice_Question_2.py
@@ -0,0 +1,6 @@
+# Create a program that takes a user's name and age as input and prints a greeting message
+
+name = str(input("Enter name: "))
+age = int(input("Enter age: "))
+
+print(f"Hi, {name}. You are {age} your old.")
