a0c07bb64197084b7489046603b3dd94ee41a0dd
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-12 09:25:22 +0530

    Practice question of day 1 completed

---
+++ Python_Boss_Coder/Day_1/Practice_Question_1.py
@@ -0,0 +1,7 @@
+# Write a Python program to calculate the area of a rectangle given its length and width
+
+length = float(input("Enter length of rectangle: "))
+breadth = float(input("Enter width of rectangle: "))
+
+area = length * breadth
+print(f"Area of rectangle length {length} and width {breadth} is {area}.")
